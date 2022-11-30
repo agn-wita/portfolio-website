30b867b5af3bb1189d0b1fdd3aa529e13c707e24
agn-wita <agn.wita@gmail.com>
2022-11-30 13:19:22 +0100

    Added JavaScript to close navigation

---
+++ script.js
@@ -0,0 +1,5 @@
+const closeButton = document.querySelector(".close-nav");
+const nav = document.querySelector(".nav");
+closeButton.addEventListener("click", () => {
+    nav.classList.remove("navigation-open");
+});
